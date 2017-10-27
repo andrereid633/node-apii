f30cd87efe7a0255a8da5d70c8cde7f942ec7e68
andrereid633 <you@example.com>
2017-10-27 15:06:13 -0500

    Task list added

---
+++ models/Task.js
@@ -0,0 +1,20 @@
+var db = require('../dbconnection.js');
+
+var Task={
+    getAllTask:function(callback){
+        return db.query("SELECT * FROM Task");
+    },
+    getTaskByID:function(id, callback){
+        return db.query("SELECT * FROM Task WHERE Id = ?", [id], callback);
+    },
+    addTask:function(Task, callback){
+        return db.query("INSERT INTO Task VALUES(?,? ?)", [Task.id, Task.Title, Task.Status],, callback);
+    },
+    deleteTask:function(id, callback){
+        return db.query("DELETE FROM Task WHERE id = ?", [id], callback);
+    },
+    updateTask:function(id, Task, callback){
+        return db.query("UPDATE Task set Title = ?, Status = ? WHERE Id = ?", [Task.Title, Task.Status, is], callback);
+    }
+};
+module.exports = Task;
